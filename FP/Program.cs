796a9ca455350136b30c0dafd2fe4766034c9258
anht31 <anht31@gmail.com>
2024-06-03 14:30:42 +0000

    add console test project

---
+++ FP/Program.cs
@@ -0,0 +1,28 @@
+﻿using LaYumba.Functional;
+
+using static System.Console;
+using Name = System.String;
+using Greeting = System.String;
+using PersonalizedGreeting = System.String;
+
+public class GeneralizingPartial
+{
+    public void Run()
+    {
+        var greet = (Greeting gr, Name name) => $"{gr}, {name}";
+
+        Name[] names = { "Tritan", "Ivan" };
+
+        var greetInformally = greet.Apply("Hey");
+        names.Map(greetInformally).ForEach(WriteLine);
+    }
+}
+
+public class Program
+{
+   public static void Main()
+   {
+      new GeneralizingPartial().Run();
+      Console.ReadLine();
+   }
+}
