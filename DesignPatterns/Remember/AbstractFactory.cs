76304f644962b734eb2c7f65b8746453229e9bf7
anht31 <anht31@gmail.com>
2024-12-20 10:35:17 +0700

    update abstract

---
+++ DesignPatterns/Remember/AbstractFactory.cs
@@ -0,0 +1,62 @@
+﻿using static System.Console;
+namespace DesignPatterns.Remember.AbstractFactory;
+
+interface IAbstractFactory
+{
+    IButton CreateButton();
+    ICheckbox CreateCheckbox();
+}
+class WindowFactory : IAbstractFactory
+{
+    public IButton CreateButton() => new WindowButton();
+
+    public ICheckbox CreateCheckbox() => new WindowCheckbox();
+}
+class MacFactory : IAbstractFactory
+{
+    public IButton CreateButton() => new MacButton();
+
+    public ICheckbox CreateCheckbox() => new MacCheckbox();
+}
+
+interface IButton
+{
+    void Render();
+}
+class WindowButton : IButton
+{
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+class MacButton : IButton
+{
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+
+interface ICheckbox
+{
+    void Render();
+}
+class WindowCheckbox : ICheckbox
+{
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+class MacCheckbox : ICheckbox
+{
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+
+class App
+{
+    IAbstractFactory Config(string os = "Window") => os switch
+    {
+        "Window" => new WindowFactory(),
+        "Mac" => new MacFactory(),
+        _ => throw new NotImplementedException(),
+    };
+    public void Run()
+    {
+        IAbstractFactory factory = Config();
+        factory.CreateButton().Render();
+        factory.CreateCheckbox().Render();
+    }
+}
---
+++ DesignPatterns/Remember/FactoryMethod.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+namespace DesignPatterns.Remember.FactoryMethod;
+
+abstract class Factory
+{
+    public abstract IProduct Create();
+    public void Render() => Create().Do();
+}
+class FactoryWindow : Factory
+{
+    public override IProduct Create() => new ConcreateWindow();
+}
+class FactoryMac : Factory
+{
+    public override IProduct Create() => new ConcreteMac();
+}
+
+interface IProduct
+{
+    void Do();
+}
+class ConcreateWindow : IProduct
+{
+    public void Do() => WriteLine($"{this.GetType()} Do Something");
+}
+class ConcreteMac : IProduct
+{
+    public void Do() => WriteLine($"{this.GetType()} Do Something");
+}
+
+
+class App
+{
+    Factory Config(string os = "Window") => os switch
+    {
+        "Window" => new FactoryWindow(),
+        "Mac" => new FactoryMac(),
+        _ => throw new NotImplementedException()
+    };
+    public void Run()
+    {
+        Factory factory = Config("Mac");
+        factory.Render();
+    }
+}
