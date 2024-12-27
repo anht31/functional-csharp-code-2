a71013a4d692336656fa622f0d3c54f0d86bca8a
anht31 <anht31@gmail.com>
2024-12-28 02:13:23 +0700

    Prototype singleton

---
+++ DesignPatterns/Remember/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+namespace DesignPatterns.Remember.Singleton;
+
+class Singleton
+{
+    private static readonly object _lock = new object();
+    private static Singleton _instance;
+    private Singleton()
+    {
+    }
+    public static Singleton GetInstance()
+    {
+        lock (_lock)
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+        }
+        return _instance;
+    }
+}
+
+class Client
+{
+    public void Run()
+    {
+        var singleton = Singleton.GetInstance();
+    }
+}
