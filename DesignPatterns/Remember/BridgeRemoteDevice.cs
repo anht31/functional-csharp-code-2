928fc98be1e65c5fa186c47306c59d33e5feb2c8
Anh Tran X <atranx@deloitte.ca>
2025-02-07 17:12:38 +0700

    Update BridgeRemoteDevice.cs

--- DesignPatterns/Remember/BridgeRemoteDevice.cs
+++ DesignPatterns/Remember/BridgeRemoteDevice.cs
@@ -1,8 +1,7 @@
 ﻿using static System.Console;
 namespace DesignPatterns.Remember.BridgeRemoteDevice;
 
-class Remote
-{
+class Remote {
     protected IDevice _device;
     public Remote(IDevice device) => _device = device;
     public void VolumeUp() {
@@ -10,32 +9,26 @@ class Remote
         _device.SetVolume(++curentVolume);
     }
 }
-class AdvancedRemote : Remote
-{
+class AdvancedRemote : Remote {
     public AdvancedRemote(IDevice device) : base(device) { }
     public void Mute() => _device.SetVolume(0);
 }
 
-interface IDevice
-{
+interface IDevice {
     int GetVolume();
     void SetVolume(int level);
 }
-class Tv : IDevice
-{
+class Tv : IDevice {
     public int GetVolume() => new Random().Next();
     public void SetVolume(int level) => WriteLine($"Volume up to {level}");
 }
-class Radio : IDevice
-{
+class Radio : IDevice {
     public int GetVolume() => new Random().Next();
     public void SetVolume(int level) => WriteLine($"Volume up to {level}");
 }
 
-class Client
-{
-    public void Run()
-    {
+class Client {
+    public void Run() {
         var tv = new Tv();
         var remote = new Remote(tv);
         remote.VolumeUp();
