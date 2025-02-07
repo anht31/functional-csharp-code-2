da6dcf7cfa59b2e1565a11758c2ed36ffec00a8b
Anh Tran X <atranx@deloitte.ca>
2025-02-07 17:23:41 +0700

    Update AbstractFactoryBridge.cs

--- DesignPatterns/RelatePatterns/AbstractFactoryBridge.cs
+++ DesignPatterns/RelatePatterns/AbstractFactoryBridge.cs
@@ -1,3 +1,48 @@
 ﻿using static System.Console;
 namespace DesignPatterns.RelatePatterns.AbstractFactoryBridge;
 
+interface IAbstractFactory {
+    IButton CreateButton();
+    ICheckbox CreateCheckbox();
+}
+class WindowFactory : IAbstractFactory {
+    public IButton CreateButton() => new WindowButton();
+    public ICheckbox CreateCheckbox() => new WindowCheckbox();
+}
+class MacFactory : IAbstractFactory {
+    public IButton CreateButton() => new MacButton();
+    public ICheckbox CreateCheckbox() => new MacCheckbox();
+}
+
+interface IButton {
+    void Render();
+}
+class WindowButton : IButton {
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+class MacButton : IButton {
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+
+interface ICheckbox {
+    void Render();
+}
+class WindowCheckbox : ICheckbox {
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+class MacCheckbox : ICheckbox {
+    public void Render() => WriteLine($"{this.GetType()} Rendering...");
+}
+
+class App {
+    IAbstractFactory Config(string os = "Window") => os switch {
+        "Window" => new WindowFactory(),
+        "Mac" => new MacFactory(),
+        _ => throw new NotImplementedException(),
+    };
+    public void Run() {
+        IAbstractFactory factory = Config();
+        factory.CreateButton().Render();
+        factory.CreateCheckbox().Render();
+    }
+}
