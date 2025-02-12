286cf60fef600bbaa0360bf48a3a9cb5d9c466bd
Anh Tran X <atranx@deloitte.ca>
2025-02-12 14:51:25 +0700

    Update BuilderComposite.cs

--- DesignPatterns/RelatePatterns/BuilderComposite.cs
+++ DesignPatterns/RelatePatterns/BuilderComposite.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using static System.Console;
 namespace DesignPatterns.RelatePatterns.BuilderComposite;
 
 interface IComponent {
@@ -7,54 +9,82 @@ interface IComponent {
 class Leaf(string name) : IComponent {
     public void Excute() => WriteLine($"{name} Do some work");
 }
-class Composite : IComponent
+class Composite(string name) : IComponent
 {
     List<IComponent> _components = new List<IComponent>();
     public void Add(IComponent component) => _components.Add(component);
     public void Remove(IComponent component) => _components.Remove(component);
     public List<IComponent> GetChildren() => _components.ToList();
-
-    public void Excute() => _components.ForEach(x => x.Excute());
+    public string Name => name;
+    public void Excute()
+    {
+        WriteLine($"Excute {name} Composite");
+        _components.ForEach(x => x.Excute());
+    }
 }
 
 interface IBuilder {
-    void Reset();
-    void BuildStepA();
-    void BuildStepB();
-    void BuildStepZ();
+    void Reset(string name);
+    void BuildNode(string name);
+    void BuildLeaf(string name);
+    void SetCurrent(string name);
 }
 class ConcreteBuilder : IBuilder
 {
-    private IComponent? _component;
-    public void BuildStepA() => _component = new Composite();
-
-    public void BuildStepB()
+    private IComponent _component; // TopLevel Composite
+    private Composite _current;
+    public ConcreteBuilder(string name) => _component = _current = new Composite(name);
+    public void BuildNode(string name)
     {
-        var leaf1 = new Leaf("Leaf1");
-        if (_component is Composite composite)
-            composite.Add(leaf1);
+        var newNode = new Composite(name);
+        _current.Add(newNode);
+        _current = newNode;
     }
 
-    public void BuildStepZ()
+    public void BuildLeaf(string name) => _current.Add(new Leaf(name));
+    public void Reset(string name) => _component = _current = new Composite(name);
+    public void SetCurrent(string name)
     {
-        var leaf2 = new Leaf("Leaf2");
-        if (_component is Composite composite)
-            composite.Add(leaf2);
+        var component = FindComposite(_component as Composite, name);
+        if (component is Composite composite)
+            _current = composite;
     }
-    public void Reset() => _component = new Composite();
+
+    private IComponent FindComposite(Composite composite, string name)
+    {
+        foreach (var item in composite.GetChildren())
+        {
+            if (item is Composite compositeItem)
+                return compositeItem.Name == name ? item : FindComposite(compositeItem, name);
+        }
+        return null;
+    }
+    public IComponent GetComponent() => _component;
 }
-
-
+class Director
+{
+    private IBuilder? _builder;
+    public Director(IBuilder builder) => _builder = builder;
+    public void SetBuilder(IBuilder builder) => _builder = builder;
+    public void Build()
+    {
+        _builder.BuildNode("node-lv1");
+        _builder.BuildNode("node-lv2");
+        _builder.BuildLeaf("leaf-21");
+        _builder.BuildLeaf("leaf-22");
+        _builder.SetCurrent("node-lv1");
+        _builder.BuildLeaf("leaf-11");
+    }
+}
 
 class Client
 {
     public void Run()
     {
-        var leaf2 = new Leaf("Leaf1");
-        var leaf2 = new Leaf("Leaf2");
-        var composite = new Composite();
-        composite.Add(leaf1);
-        composite.Add(leaf2);
-        composite.Excute();
+        var builder = new ConcreteBuilder("root");
+        var director = new Director(builder);
+        director.Build();
+        var product = builder.GetComponent();
+        product.Excute();
     }
 }
