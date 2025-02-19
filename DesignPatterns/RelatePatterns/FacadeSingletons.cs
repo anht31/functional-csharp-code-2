b14345d52646b081f79df265e314a581652a6204
Anh Tran X <atranx@deloitte.ca>
2025-02-19 17:43:25 +0700

    Create FacadeSingletons.cs

---
+++ DesignPatterns/RelatePatterns/FacadeSingletons.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+namespace DesignPatterns.RelatePatterns.FacadeSingletons;
+
+class OtherClass
+{
+    public void DoSomething() { }
+    public void DoAnother() { }
+}
+
+class AdditionalFacade
+{
+    public void AnotherOperation() { }
+}
+
+class Facade
+{
+    private static Facade Instance;
+    OtherClass _otherClass;
+    AdditionalFacade _additionalFacade;
+    private Facade(OtherClass otherClass, AdditionalFacade additionalFacade)
+    {
+        _otherClass = otherClass;
+        _additionalFacade = additionalFacade;
+    }
+    public static Facade GetInstance() => Instance ??= new Facade(new OtherClass(), new AdditionalFacade());
+    public void Execute()
+    {
+        _otherClass.DoSomething();
+        _otherClass.DoAnother();
+        _additionalFacade.AnotherOperation();
+    }
+}
