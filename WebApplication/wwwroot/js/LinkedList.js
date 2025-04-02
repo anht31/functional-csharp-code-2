3de892a5b961da428d5687cf1eb6b58d42a843b4
Anh Tran X <atranx@deloitte.ca>
2025-04-02 17:38:22 +0700

    Update LinkedList.js

--- WebApplication/wwwroot/js/LinkedList.js
+++ WebApplication/wwwroot/js/LinkedList.js
@@ -152,10 +152,118 @@ class LinkedList {
     }
 }
 
+class DoubleLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        this.tail = this.head // ref to current tail (use this as single access for LinkedList)
+        this.length = 1
+    }
+
+    append(value) {
+        const newNode = { value, next: null, prev: null }
+        const current = this.tail
+        current.next = newNode
+        newNode.prev = current
+        this.tail = newNode
+        this.length++
+    }
+
+    prepend(value) {
+        const newNode = { value, next: null, prev: null }
+        this.head.prev = newNode
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+    }
+
+    insert(index, value) {
+        if (index < 0) return
+        if (index > this.length) {
+            this.append(value)
+            return
+        }
+        if (index == 0) {
+            this.prepend(value)
+            return
+        }
+
+        const leader = this.traverseToIndex(index - 1)
+        const newNode = { value, next: null }
+
+        // 1 -> [newNode] -> 2
+        const holdingPoint = leader.next
+        leader.next = newNode
+        newNode.next = holdingPoint
+        this.length++
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length)
+            return
+
+        if (index === 0) {
+            this.head = this.head.next
+            return
+        }
+
+        console.log(this.head)
+
+        const leader = this.traverseToIndex(index - 1)
+        //const holdingPoint = leader.next?.next
+        //delete leader.next // no need delete, because no pointer wilL GC
+        const unwantedNode = leader.next
+        leader.next = unwantedNode.next
+        this.length--
+    }
+
+    traverseToIndex(index) {
+        let currentNode = this.head
+        let currentIndex = 0
+        while (currentIndex < index && currentNode != null) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode
+    }
+
+    printList() {
+        const array = []
+        let currentNode = this.head
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+
+    demo() {
+        // 1 -> 10 --> 5 --> 16
+        let myLinkedList = new DoubleLinkedList(10)
+        myLinkedList.append(5)
+        myLinkedList.append(16)
+        myLinkedList.prepend(1)
+        console.log(myLinkedList.printList())
+        console.log(myLinkedList)
+
+        // 1 -> 10 --> [99] --> 5 --> 16
+        //myLinkedList.insert(2, 99)
+        //console.log(myLinkedList.printList())
+
+        //console.log(myLinkedList)
+        //myLinkedList.remove(2)
+        //console.log(myLinkedList.printList())
+
+    }
+}
+
 
 class App {
     run = () => {
-        new LinkedList().demo()
+        new DoubleLinkedList().demo()
     }
 }
 
