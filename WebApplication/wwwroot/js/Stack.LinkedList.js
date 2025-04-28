0ca503e100ac17bd30b1a113254659e52abcef2d
anht31 <anht31@gmail.com>
2025-04-28 22:35:35 +0700

    stack Linkedlist

--- WebApplication/wwwroot/js/Stack.LinkedList.js
+++ WebApplication/wwwroot/js/Stack.LinkedList.js
@@ -13,20 +13,47 @@ class Stack {
     }
 
     peek() {
+        return !this.$isEmpty() ? this.top?.value : null 
     }
 
     push(value) {
+        const previewNode = this.top
+        const newNode = new Node(value)
+        newNode.next = previewNode
+        this.top = newNode
+        this.bottom ??= newNode
+        this.length++
     }
 
     pop() {
+        if (this.$isEmpty()) return null
+
+        const node = this.top
+        const nextNode = node.next
+        this.top = nextNode
+        this.bottom = nextNode === null ? null : this.bottom
+        this.length--
+
+        return node?.value
     }
 
-    //isEmpty
+    $isEmpty = () => this.top === null
 }
 
 class App {
     run() {
         const myStack = new Stack();
+        myStack.push('google')
+        myStack.push('udemy')
+        myStack.push('discord')
+        console.log(myStack)
+        console.log(myStack.peek())
+        console.log(myStack.pop())
+        console.log(myStack.pop())
+        console.log(myStack) // why this time this.top is null
+        console.log(myStack.pop())
+        console.log(myStack.pop())
+        console.log(myStack)
     }
 }
 
